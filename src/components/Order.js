56c89a14fded721af016364f35f3cc09d40f67d2
unknown <keerthivarshini.ramesh@gmail.com>
2025-01-18 12:17:05 +0530

    code updated

--- src/components/Order.js
+++ src/components/Order.js
@@ -37,7 +37,7 @@ export default function Order() {
             setView(one)
             console.log(view)
             setdataView(true)
-            
+
         }
 
     }
@@ -161,7 +161,7 @@ export default function Order() {
 
                     <div className="mb-3 buttons">
                         <button className="btn btn-primary text-center create" onClick={() => setdataView(false)}>Cancel</button>
-                        <button className="btn btn-primary float-end create" onClick={(e) => Save(e)} >Save</button>
+                        <button className="btn btn-primary " onClick={(e) => Save(e)} >Save</button>
                     </div>
 
 
